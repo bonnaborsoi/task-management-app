5b82392fdcacdaf48ff33f77310cc71a89e928fe
bonnaborsoi <bonnagilborsoi@gmail.com>
2024-08-21 00:25:35 -0300

    clean main

--- app/src/main/java/com/example/task_management_app/data/firebase/FirebaseService.kt
+++ app/src/main/java/com/example/task_management_app/data/firebase/FirebaseService.kt
@@ -12,23 +12,19 @@ class FirebaseService {
         return database.getReference(path)
     }
 
-    // Gravar dados no Realtime Database
     suspend fun <T> setValue(reference: DatabaseReference, data: T) {
         reference.setValue(data).await()
     }
 
-    // Ler dados de uma referência
     suspend inline fun <reified T> getValue(reference: DatabaseReference): T? {
         val snapshot = reference.get().await()
         return snapshot.getValue(T::class.java)
     }
 
-    // Atualizar dados no Realtime Database
     suspend fun <T> updateValue(reference: DatabaseReference, data: Map<String, T>) {
         reference.updateChildren(data).await()
     }
 
-    // Deletar dados de uma referência
     suspend fun deleteValue(reference: DatabaseReference) {
         reference.removeValue().await()
     }
